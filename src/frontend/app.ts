be35323f8d538a26c9063ca792411f3e305dc8f1
Martin Sigloch <f0i.git@projects.f0i.de>
2024-10-27 21:44:04 +0100

    Handle error when no info-tab is active

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -8,14 +8,17 @@ const IDENTITY_PROVIDER = "https://login.f0i.de";
 
 window.onload = () => {
   console.log("onload: opener:", window.opener);
-
   if (window.opener) {
     initICgsi(GSI_CLIENT_ID);
   } else {
     initDemo(IDENTITY_PROVIDER);
   }
   document.getElementById("version")!.innerText = process.env.BUILD_TIME!;
-  (window as any).showInfo(document.location.hash.substring(1));
+  try {
+    (window as any).showInfo(document.location.hash.substring(1));
+  } catch (e) {
+    // ignore
+  }
 };
 
 (window as any).showInfo = function (sectionId: string) {
@@ -24,7 +27,6 @@ window.onload = () => {
   document.getElementById("help")!.style.display = "none";
   document.getElementById("security")!.style.display = "none";
   document.getElementById("about")!.style.display = "none";
-
   // Show the selected section
   document.getElementById("info")!.style.display = "block";
   document.getElementById(sectionId)!.style.display = active ? "none" : "block";
